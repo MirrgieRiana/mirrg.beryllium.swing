6b88c4477267795394ea4933067570c5dd4feb3b
Mirrgie Riana <tacticsrealize2@gmail.com>
2018-04-26 14:38:12 +0900

    add: old src

---
+++ src/test/java/mirrg/beryllium/swing/SampleTitledGroup.java
@@ -0,0 +1,37 @@
+package mirrg.beryllium.swing;
+
+import java.awt.BorderLayout;
+import java.awt.CardLayout;
+import java.util.Vector;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+import javax.swing.JList;
+
+import backup.TitledGroup;
+
+public class SampleTitledGroup
+{
+
+	public static void main(String[] args)
+	{
+		new Frame() {
+
+			@Override
+			protected void init()
+			{
+				setLayout(new BorderLayout());
+				{
+					TitledGroup titledGroup = new TitledGroup("Title");
+					titledGroup.setLayout(new CardLayout());
+					titledGroup.add(createScrollPane(new JList<String>(IntStream.range(0, 20)
+						.mapToObj(i -> "" + i)
+						.collect(Collectors.toCollection(Vector::new))), 200, 200));
+					add(titledGroup);
+				}
+			}
+
+		}.setVisible(true);
+	}
+
+}
