97ff6d603fa505a8a0c211942012d69c32fe8a42
Mirrgie Riana <tacticsrealize2@gmail.com>
2018-04-26 15:06:33 +0900

    move: SplitPane util

--- src/test/java/backup/HSwing.java
+++ src/test/java/backup/HSwing.java
@@ -23,7 +23,6 @@ import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
-import javax.swing.JSplitPane;
 import javax.swing.border.EmptyBorder;
 import javax.swing.event.DocumentEvent;
 import javax.swing.event.DocumentListener;
@@ -31,56 +30,6 @@ import javax.swing.text.JTextComponent;
 
 public class HSwing
 {
-
-	public static Component createSplitPaneHorizontal(Component... components)
-	{
-		return createSplitPaneHorizontal(Arrays.asList(components));
-	}
-
-	public static Component createSplitPaneHorizontal(List<Component> components)
-	{
-		if (components.size() == 1) return components.get(0);
-		return new JSplitPane(JSplitPane.HORIZONTAL_SPLIT, true,
-			components.get(0),
-			createSplitPaneHorizontal(components.subList(1, components.size())));
-	}
-
-	public static JSplitPane createSplitPaneHorizontal(Component c1, Component c2)
-	{
-		return new JSplitPane(JSplitPane.HORIZONTAL_SPLIT, true, c1, c2);
-	}
-
-	public static JSplitPane createSplitPaneHorizontal(double resizeWeight, Component c1, Component c2)
-	{
-		JSplitPane splitPane = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT, true, c1, c2);
-		splitPane.setResizeWeight(resizeWeight);
-		return splitPane;
-	}
-
-	public static Component createSplitPaneVertical(Component... components)
-	{
-		return createSplitPaneVertical(Arrays.asList(components));
-	}
-
-	public static Component createSplitPaneVertical(List<Component> components)
-	{
-		if (components.size() == 1) return components.get(0);
-		return new JSplitPane(JSplitPane.VERTICAL_SPLIT, true,
-			components.get(0),
-			createSplitPaneVertical(components.subList(1, components.size())));
-	}
-
-	public static JSplitPane createSplitPaneVertical(Component c1, Component c2)
-	{
-		return new JSplitPane(JSplitPane.VERTICAL_SPLIT, true, c1, c2);
-	}
-
-	public static JSplitPane createSplitPaneVertical(double resizeWeight, Component c1, Component c2)
-	{
-		JSplitPane splitPane = new JSplitPane(JSplitPane.VERTICAL_SPLIT, true, c1, c2);
-		splitPane.setResizeWeight(resizeWeight);
-		return splitPane;
-	}
 
 	public static Component createBorderPanelUp(Component... components)
 	{
