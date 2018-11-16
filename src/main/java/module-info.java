5883b2b4fd0d0227e74a6c8c66c0b4e4d947fd15
Mirrgie Riana <tacticsrealize2@gmail.com>
2018-11-16 21:41:49 +0900

    Javaバージョンを1.8に低下

--- src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module mirrg.beryllium.swing
-{
-	exports mirrg.beryllium.swing;
-	exports mirrg.beryllium.swing.components;
-	exports mirrg.beryllium.swing.util;
-
-	requires transitive java.desktop;
-	requires static org.junit.jupiter.api;
-}
